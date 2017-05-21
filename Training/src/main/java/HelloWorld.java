5638eeecfb129828c6263d6c3bee83c53c9e8044
Piotr Pawlik <piotr.pawlik@smart4aviation.aero>
2017-05-21 16:50:47 +0200

    First commit

---
+++ Training/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by holkins on 2017-05-18.
+ */
+public class HelloWorld {
+
+    public static void main (String args[]) throws IOException {
+        System.out.println("Hello world");
+        System.out.println("Please press Enter to close");
+        System.in.read();
+    }
+
+}
