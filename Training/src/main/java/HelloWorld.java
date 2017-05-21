432fde2ff21e33028a96ffac7a29912d55133f58
Piotr Pawlik <piotr.pawlik@smart4aviation.aero>
2017-05-21 21:04:03 +0200

    fixed zadanie1

--- Training/src/main/java/HelloWorld.java
+++ Training/src/main/java/HelloWorld.java
@@ -1,14 +1,24 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 /**
  * Created by holkins on 2017-05-18.
  */
 public class HelloWorld {
 
-    public static void main (String args[]) throws IOException {
-        System.out.println("Hello world");
-        System.out.println("Please press Enter to close");
-        System.in.read();
-    }
+    public static void main(String args[]) throws IOException {
 
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+
+        while(true) {
+            System.out.println("Please write 'exit' and tap ENTER to close");
+
+            if (userInput.next().equals("exit")) {
+                System.exit(0);
+            }
+        }
+
+
+    }
 }
